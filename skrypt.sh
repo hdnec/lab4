1e4ec8a10522b41890040f7d2adc90fa4f3d39d9
Mykyta <dsw49351@student.dsw.edu.pl>
2023-04-23 23:58:16 +0200

    --logs

--- skrypt.sh
+++ skrypt.sh
@@ -6,3 +6,12 @@ else
   echo "Nieznana opcja: $1"
   exit 1
 fi
+
+if [ "$1" = "--logs" ]; then
+  for i in $(seq 1 100); do
+    echo "Nazwa pliku: log$i.txt" > log$i.txt
+    echo "Nazwa skryptu: skrypt.sh" >> log$i.txt
+    echo "Data: $(date)" >> log$i.txt
+  done
+  exit 0
+fi
